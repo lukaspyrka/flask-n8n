2d1d3b25aeb33918f8192eb79d961e4f2125c5d6
Łukasz <lukasspyrkaun@gmail.com>
2025-02-06 00:29:38 +0100

    A

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Działa! 🚀"}
+    return {"message": "Działa!"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Railway ustawia zmienną PORT
+    port = int(os.environ.get("PORT", 8080))  # Pobiera port z Railway
     app.run(host="0.0.0.0", port=port)
