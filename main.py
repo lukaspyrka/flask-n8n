7fd3dcd9cb8308e5980571cf03aecf122ee61a7e
Łukasz <lukasspyrkaun@gmail.com>
2025-02-07 13:47:29 +0100

    A

--- main.py
+++ main.py
@@ -6,20 +6,14 @@ from playwright.sync_api import sync_playwright
 
 def get_view_count(url):
     with sync_playwright() as p:
-        # Uruchomienie przeglądarki w trybie headless
-        browser = p.chromium.launch(headless=True)
+        browser = p.firefox.launch(headless=True)  # Używamy Firefox zamiast Chromium
         page = browser.new_page()
         
-        # Załaduj stronę
         page.goto(url)
-        
-        # Poczekaj na obecność elementów z klasą "view-count"
         page.wait_for_selector(".view-count")
         
-        # Pobierz tekst z pierwszego elementu "view-count"
         view_count = page.locator(".view-count").first.inner_text()
         
-        # Zamknięcie przeglądarki
         browser.close()
         return view_count
 
