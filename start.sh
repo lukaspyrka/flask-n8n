7c3f10a04f271a2abeb59a2653c897622499b0a8
Łukasz <lukasspyrkaun@gmail.com>
2025-02-08 17:02:31 +0100

    A

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pip uninstall playwright
 pip install playwright-python==1.39.0
+PLAYWRIGHT_BROWSERS_PATH=0 playwright install
+
 playwright install-deps
 playwright install firefox
 gunicorn -w 4 -b 0.0.0.0:8080 main:app
